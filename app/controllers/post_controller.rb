d71894795768d337e0c71d5ae802802af4d8cd32
Andre Ferreira <alflol@gmail.com>
2016-04-08 07:53:32 +0100

    initial commit

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,36 @@
+class PostController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(2)
+  end
+
+  def create
+    @post = Post.new(:text => params[:post][:text])
+    if @post.save
+      flash[:notice] = "Post gravado"
+      redirect_to post_index_path
+    else
+      flash[:notice] = "Erro ao gravar post"
+      redirect_to post_index_path
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(params[:post])
+      flash[:notice] = "Update sucessfull"
+      redirect_to post_path
+    else
+      flash[:notice] = "Update sucessfull"
+      render 'edit'
+    end
+  end
+
+end
---
+++ db/migrate/20160407140939_remove_username_post.rb
@@ -0,0 +1,5 @@
+class RemoveUsernamePost < ActiveRecord::Migration
+  def change
+    remove_column :posts, :username, :string
+  end
+end
